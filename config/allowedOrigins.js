06812d511aa275c69f8c87c2719778c7d0b9b773
Adarsh Nath Tripatthi <121425506+tripathijee7@users.noreply.github.com>
2024-07-25 22:13:25 +0530

    Add files via upload

---
+++ config/allowedOrigins.js
@@ -0,0 +1,9 @@
+
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:4200',
+    'https://kaleidoscopic-gnome-1f4673.netlify.app'
+
+];
+
+module.exports = allowedOrigins;
---
+++ routes/userRoutes.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+
+const router = express.Router();
+const userController = require('../controllers/usersController');
+const verifyJWT = require("../middleware/verifyJWT");
+//authentication
+// /api/users/login
+router.post('/users', userController.registerUser);
+
+router.post('/users/login', userController.userLogin);
+
+
+// /api/users
+
+
+//get the current user
+router.get('/user', verifyJWT, userController.getCurrentUser);
+
+
+//update the user
+router.put('/user',verifyJWT, userController.updateUser);
+
+
+module.exports = router;
